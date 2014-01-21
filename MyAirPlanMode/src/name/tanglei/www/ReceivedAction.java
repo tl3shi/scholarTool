f16dfa3b1fb410983cf1cba0491901a7f1faa00b
tanglei <mac@tanglei.name>
2014-01-22 01:53:42 +0800

    time count down

--- MyAirPlanMode/src/name/tanglei/www/ReceivedAction.java
+++ MyAirPlanMode/src/name/tanglei/www/ReceivedAction.java
@@ -1,12 +1,18 @@
 package name.tanglei.www;
 
+import java.lang.ref.WeakReference;
+
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class ReceivedAction extends Activity
@@ -57,12 +63,15 @@ public class ReceivedAction extends Activity
 			Toast.makeText(ReceivedAction.this, ReceivedAction.this.getString(R.string.cancelAirmodeToast), Toast.LENGTH_SHORT).show();
 		}
 	};
+
+	private AlertDialog dialog;
+	private Button positiveButton;
 	
 	public  void showAlertDialog(Context context, String title,
 			String content, int positive_id, int negative_id, DialogInterface.OnClickListener okListener, 
 			DialogInterface.OnClickListener cancelListener, int delay_seconds)
 	{
-		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+		Builder alertDialogBuilder = new AlertDialog.Builder(
 				context);
 		alertDialogBuilder.setTitle(title);
 		alertDialogBuilder.setMessage(content);
@@ -70,24 +79,57 @@ public class ReceivedAction extends Activity
 				context.getString(positive_id), okListener);
 		alertDialogBuilder.setNegativeButton(
 				context.getString(negative_id), cancelListener);
-		final AlertDialog ad = alertDialogBuilder.create();
-		ad.show();
-		
-		android.os.Handler hander= new android.os.Handler();
-	    hander.postDelayed(new Runnable() 
-	    {     
-	     @Override
-	     public void run() 
-	     {
-	    	 if(ad.isShowing())//the user does not take action.
-	    	 {
-	    		 ad.dismiss();
-	    		 setAirPlaneState(true);
-	    		 ReceivedAction.this.finish();
-	    	 }
-	     }
-	    }, delay_seconds * 1000);
+		dialog = alertDialogBuilder.create();
+		dialog.show();
+		positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+		countDownTimeHander.sendEmptyMessageDelayed(0, 100);
 	}
+	
+	//http://ifoggy.iteye.com/blog/1874499
+	//or else This Handler class should be static or leaks might occur
+	static class MsgHandler extends Handler 
+	{  
+	    private WeakReference<ReceivedAction> mActivity;  
+	  
+	    MsgHandler(ReceivedAction activity) {  
+	        mActivity = new WeakReference<ReceivedAction>(activity);  
+	    }  
+	  
+	    @Override  
+	    public void handleMessage(Message msg) {  
+	    	ReceivedAction activity = mActivity.get();  
+	        if (activity != null) {  
+	            activity.handleMessage(msg);  
+	        }  
+	    }  
+	}  
+	  
+	private Handler countDownTimeHander = new MsgHandler(this); 
+	
+	public void handleMessage(Message msg)
+	{
+		if(delaycount >= 0)
+		{
+			if(dialog != null)
+			{
+				String txt = this.getString(R.string.confirmAirmodeButtonOK) + "(" + delaycount + " s )";
+				positiveButton.setText(txt);
+				countDownTimeHander.sendEmptyMessageDelayed(0, 1000); 
+			}
+			delaycount --;
+		}else
+		{
+			
+			if(dialog.isShowing())//user does not action
+			{
+				ReceivedAction.this.finish();
+				this.setAirPlaneState(true);
+				dialog.dismiss();
+			}
+		}
+	}
+	
+	private int delaycount = 5;
 	
 	public void setAirPlaneState(boolean state)
 	{
